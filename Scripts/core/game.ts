74ce7e9db64ffa84f47e9b4e3a71836819d3970c
Arreth45 <arreth45@gmail.com>
2016-02-01 18:03:05 -0500

    added notes

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,6 +1,11 @@
 /// <reference path="_reference.ts"/>
 
 // MAIN GAME FILE
+// Jake Parnell
+// Comp392 - Assignment 1
+// Last Modified by: Jake Parnell
+// Date Last Modified Jan 28th 2016
+
 
 // THREEJS Aliases
 import Scene = THREE.Scene;
--- Scripts/objects/control.ts
+++ Scripts/objects/control.ts
@@ -1,5 +1,10 @@
 /// <reference path="../../typings/tsd.d.ts"/>
 
+// Jake Parnell
+// Comp392 - Assignment 1
+// Last Modified by: Jake Parnell
+// Date Last Modified Jan 28th 2016
+
 module objects {
     // CONTROL CLASS ++++++++++++++++++++++++++++++++++++++++++
     export class Control { 
